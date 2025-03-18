cda404136cf869f0b8bf12c2658d4400d3819b1d
Krolon <tomaszekograbek10@wp.pl>
2025-03-18 20:45:33 +0100

    Update StormSL.lua
    split clamp into 2 variants
    
    split fixNan into 2 variants and added checkNan + fixInf and checkInf
    
    fixed lerpT argument names
    added inverses of lerps
    
    split xorshift into 32, 64 and variable variants
    
    split rng into 2 variants: closure and class
    
    corrected some documentation

--- StormSL.lua
+++ StormSL.lua
@@ -8,18 +8,28 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 
 	---@class StormSL
 	---@field version_SL string
-	---@field clamp_SL fun(minimum:number, maximum:number, value:number):number
+	---@field clampS_SL fun(minimum:number, maximum:number, value:number):number
+	---@field clampQ_SL fun(minimum:number, maximum:number, value:number):number
 	---@field ewma_SL fun(old:number, new:number, gamma:number):number
 	---@field ewmaClass_SL fun(alpha:number?, innitialValue:number?):table
 	---@field ewmaClosure_SL fun(alpha:number?, value:number?):fun(newValue:number):number
 	---@field threshold_SL fun(x:number, min:number, max:number):boolean
-	---@field fixNaN_SL fun(x:number, fixValue:number):any
+	---@field fixNanTo0_SL fun(value:number):number
+	---@field fixNanToAny_SL fun(value:number, fixValue:number):any
+	---@field checkNan_SL fun(value:number):boolean
+	---@field fixInfToAny_SL fun(value:number, fixValue:number):number
+	---@field checkInf_SL fun(value:number):boolean
 	---@field lerpX_SL fun(x:number, x1:number, y1:number, x2:number, y2:number):number
-	---@field lerpT_SL fun(t:number, t0_value:number, t1_value:number):number
+	---@field lerpT_SL fun(t:number, l:number, h:number):number
+	---@field inverseLerpT_SL fun(v:number, l:number, h:number):number
+	---@field inverseLerpX_SL fun(y:number, x1:number, y1:number, x2:number, y2:number):number
 	---@field simpleCopy_SL fun(tableIn:table, tableOut:table?, allowOverwrite:boolean?):table
 	---@field deepCopy_SL fun(tableIn:table, tableOut:table?):table
-	---@field xorShift_SL fun(int64:integer):integer
-	---@field createRngClosure_SL fun(seed:integer?, boundLow:number?, boundHigh:number?, integerMode:boolean?):function
+	---@field xorShift64_SL fun(int64:integer):integer
+	---@field xorShift32_SL fun(int32:integer):integer
+	---@field xorShift_SL fun(int:integer, l1:integer, r2:integer, l3:integer):integer
+	---@field createRngClosure_SL fun(seed:integer?, boundLow:number?, boundHigh:number?, integerMode:boolean?):fun(newSeed:integer?):number
+	---@field createRngClass_SL fun(seed:integer?, boundLow:number?, boundHigh:number?, integerMode:boolean?):table
 	---@field stringToWordTable_SL fun(string:string):table
 	---@field getAverage_SL fun(...:number):number
 	---@field Vector table Vector---The standard library for Stormworks Lua.
@@ -30,24 +40,35 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 		---@endsection
 
 		---@section clamp_SL
+		---a couple chars smaller than clamQ but twice as slow which won't matter in casual usage
 		---@param minimum number
 		---@param maximum number
 		---@param value number
 		---@return number
-		clamp_SL = function(minimum, maximum, value)
-			--using ternary is faster than if-else or math.min/max
-			return value > maximum and maximum or value < minimum and minimum or value
+		clampS_SL = function(minimum, maximum, value)
+			return math.min(maximum, math.max(minimum, value))
+		end,
+		---@endsection
+
+		---@section clamp_SL
+		---a couple chars larger than clampS but twice as fast which won't matter in casual usage
+		---@param minimum number
+		---@param maximum number
+		---@param value number
+		---@return number
+		clampQ_SL = function(minimum, maximum, value)
+			return value > maximum and maximum or (value < minimum and minimum or value)
 		end,
 		---@endsection
 
 		---@section ewma_SL
 		---Exponential weighted moving average. Smoothes out the input number.
-		---@param old number previous smoothed number
-		---@param new number number to add and smooth, output approuches new
-		---@param gamma number how strong smoothing is
-		---@return number smoothed
-		ewma_SL = function(old, new, gamma)
-			return ( (gamma-1) * old + new) / gamma
+		---@param oldValue number previous smoothed number
+		---@param newValue number number to add and smooth, output approuches new
+		---@param alpha number how strong smoothing is
+		---@return number
+		ewma_SL = function(oldValue, newValue, alpha)
+			return (1 - alpha) * oldValue + newValue * alpha
 		end,
 		---@endsection
 
@@ -74,7 +95,7 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 			}
 		end,
 		---@endsection
-		
+
 		---@section ewmaClosure_SL
 		---@param alpha number? the smoothing factor of the EWMA, default 0.1
 		---@param value number? the INITIAL! value of the EWMA. Default to 0.
@@ -82,6 +103,8 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 		ewmaClosure_SL = function (alpha, value)
 			alpha = alpha or 0.1
 			value = value or 0
+			---@param newValue number new value to smooth
+			---@return number smoothed Output of the EWMA
 			return function(newValue)
 				value = (1 - alpha) * value + alpha * newValue
 				return value
@@ -90,7 +113,7 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 		---@endsection
 
 		---@section threshold_SL
-		---Checks if the value is between the min and max.
+		---Checks if the value is between the min and max. Exclusive.
 		---@param x number
 		---@param min number
 		---@param max number
@@ -100,19 +123,71 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 		end,
 		---@endsection
 
-		---@section fixNaN_SL
-		---Replaces NaN with an optionally provided value, or defaults it to 0.
-		---@param x number value to correct
-		---@param fixValue number? number to replace NaN with. Defaults to 0
-		---@return any y Will be x if x isn't NaN, or fixValue if it is (or 0 if one isn't provided).
-		fixNaN_SL = function(x, fixValue)
-			return x == x and x or fixValue or 0
+		---@section fixNanTo0_SL
+		---Replaces NaN with a 0.
+		---@param value number value to correct
+		---@return number corrected will be 0 if NaN, otherwise the provided value
+		fixNanTo0_SL = function(value)
+			return value ~= value and 0 or value
+		end,
+		---@endsection
+
+		---@section fixNanToAny_SL
+		---Replaces NaN with a provided value.
+		---@param value number value to correct
+		---@param fixValue any value to replace NaNs with
+		---@return any corrected will be fixValue if NaN, otherwise the provided value
+		fixNanToAny_SL = function(value, fixValue)
+			if value == value then
+				return value
+			end
+			return fixValue
+		end,
+		---@endsection
+
+		---@section checkNan_SL
+		---Checks if the value is NaN.
+		---@param value number
+		---@return boolean
+		checkNan_SL = function(value)
+			return value ~= value
+		end,
+		---@endsection
+
+		---@section fixInfToAny_SL
+		---@param value number
+		---@param fixValue number
+		---@return number
+		fixInfToAny_SL = function(value, fixValue)
+			if value == 1/0 or value == -1/0 then
+				return fixValue
+			end
+			return value
+		end,
+		---@endsection
+
+		---@section checkInf_SL
+		---@param value number
+		---@return boolean
+		checkInf_SL = function(value)
+			return value == 1/0 or value == -1/0
+		end,
+		---@endsection
+
+		---@section lerpT_SL
+		---Linearly interpolate between y1 and y2.
+		---@param t number control input
+		---@param l number output for when t is 0
+		---@param h number output for when t is 1
+		---@return number y linear interpolation between y1 for t=0 and y2 for t=1
+		lerpT_SL = function(t, l, h)
+			return l + t * (h - l)
 		end,
 		---@endsection
 
 		---@section lerpX_SL
 		---Linearly interpolate between {x1,y1} and {x2,y2}. Produces y for a given x.
-		---@param x number more commonly denoted as "t", I however used x because it's better than just working in 0-1 range
+		---@param x number more commonly denoted as "t", however x was used because it's better than just working in 0-1 range
 		---@param x1 number input 1
 		---@param y1 number output 1
 		---@param x2 number input 2
@@ -123,19 +198,30 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 		end,
 		---@endsection
 
-		---@section lerpT_SL
-		---Linearly interpolate between y1 and y2.
-		---@param t number control input
-		---@param t0_value number output for when t is 0
-		---@param t1_value number output for when t is 1
-		---@return number y linear interpolation between y1 for t=0 and y2 for t=1
-		lerpT_SL = function(t, t0_value, t1_value)
-			return t0_value + t * (t1_value - t0_value)
+		---@section inverseLerpT_SL
+		---@param v number value to find the t for
+		---@param l number lower bound
+		---@param h number higher bound
+		---@return number t value for v between l and h
+		inverseLerpT_SL = function(v, l, h)
+			return (v - l) / (h - l)
+		end,
+		---@endsection
+
+		---@section inverseLerpX_SL
+		---@param y number value to find the x for
+		---@param x1 number input 1
+		---@param y1 number output 1
+		---@param x2 number input 2
+		---@param y2 number output 2
+		---@return number x value for y between x1 and x2
+		inverseLerpX_SL = function(y, x1, y1, x2, y2)
+			return (x1*y - x1*y2 - x2*y + x2*y1) / (y1 - y2)
 		end,
 		---@endsection
 
 		---@section simpleCopy_SL
-		---Will copy the provided table and all it's nested contents including tables.
+		---Will copy the provided table and all it's nested contents including tables AS A REFERENCE! Does not deep copy!
 		---@param tableIn table table to copy
 		---@param tableOut table? table to copy into, if not provided will create a new table
 		---@param allowOverwrite boolean? if true and tableOut is present, it'll have it's indices overwritten
@@ -172,39 +258,97 @@ do	--hides the upvalues so that there's no chance of name conflict for locals be
 		end,
 		---@endsection
 
-		---@section xorShift_SL
+		---@section xorShift64_SL
+		---Xorshift random number generator for 64 bit integers. Preferable over others.
 		---@param int64 integer
 		---@return integer
-		xorShift_SL = function(int64)
+		xorShift64_SL = function(int64)
 			int64 = int64 ~ (int64 << 13)
 			int64 = int64 ~ (int64 >> 7)
 			return int64 ~ (int64 << 17)
 		end,
 		---@endsection
 
+		---@section xorShift32_SL
+		---Xorshift random number generator for 32 bit integers. xorShift64 is preferable over this one.
+		---@param int32 integer
+		---@return integer
+		xorShift32_SL = function(int32)
+			int32 = int32 & 0xFFFFFFFF
+			int32 = int32 ~ (int32 << 13)
+			int32 = int32 ~ (int32 >> 17)
+			return (int32 ~ (int32 << 5) ) & 0xFFFFFFFF
+		end,
+		---@endsection
+
+		---@section xorShift_SL
+		---Xorshift random number generator with custom shifts. xorShift64 is preferable over this one.
+		---@param int integer
+		---@param l1 integer
+		---@param r2 integer
+		---@param l3 integer
+		---@return integer
+		xorShift_SL = function(int, l1, r2, l3)
+			int = int ~ (int << l1)
+			int = int ~ (int >> r2)
+			return int ~ (int << l3)
+		end,
+		---@endsection
+
 		---@section createRngClosure_SL
-		---Creates a random number generator closure.
+		---Creates a random number generator closure. Uses xorShift64 masked to middle 32 bits.
 		---@param seed integer?
-		---@param boundLow number?
-		---@param boundHigh number?
+		---@param boundLow number? default to 0
+		---@param boundHigh number? default to 1
 		---@param integerMode boolean?
-		---@return function
+		---@return fun(newSeed:integer?):number
 		createRngClosure_SL = function(seed, boundLow, boundHigh, integerMode)
 			seed = seed or 1
 			boundLow = boundLow or 0
 			boundHigh = boundHigh or 1
 
 			--4294967295 is 2^32-1, the maximum value of a 32 bit unsigned integer
-			local xorShiftSL, floor, mask = StormSL.xorShift_SL, math.floor, 4294967295
+			local xorShiftSL, floor, mask = StormSL.xorShift64_SL, math.floor, 4294967295
 
-			return function()
-				seed = xorShiftSL(seed)
+			return function(newSeed)
+				seed = xorShiftSL(newSeed or seed)
 				local x = ( (seed >> 16) & mask) / mask
 				if integerMode then
 					return floor(boundLow + x *(boundHigh - boundLow) + 0.5)
 				end
 				return boundLow + x * (boundHigh - boundLow)
 			end
+		end,
+		---@endsection
+
+		---@section createRngClass_SL
+		---@class RNG
+		---@field seed integer the seed of the RNG
+		---@field boundLow number the lower bound of the RNG
+		---@field boundHigh number the higher bound of the RNG
+		---@field integerMode boolean if true, will return integers, otherwise floats
+		---@field generate fun(self:RNG):number generates a new random number
+		---@param seed integer? the seed of the RNG, default 1
+		---@param boundLow number? the lower bound of the RNG, default 0
+		---@param boundHigh number? the higher bound of the RNG, default 1
+		---@param integerMode boolean? if true, will return integers, otherwise floats
+		---@return table
+		createRngClass_SL = function (seed, boundLow, boundHigh, integerMode)
+			local xorShiftSL, floor, mask = StormSL.xorShift64_SL, math.floor, 4294967295
+			return {
+				seed = seed or 1,
+				boundLow = boundLow or 0,
+				boundHigh = boundHigh or 1,
+				integerMode = integerMode,
+				generate = function(self)
+					self.seed = xorShiftSL(self.seed)
+					local x = ( (self.seed >> 16) & mask) / mask
+					if self.integerMode then
+						return floor(self.boundLow + x *(self.boundHigh - self.boundLow) + 0.5)
+					end
+					return self.boundLow + x * (self.boundHigh - self.boundLow)
+				end
+			}
 		end,
 		---@endsection
 
