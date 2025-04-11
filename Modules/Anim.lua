427ade8d2ff531a59ed2a8c02f2ca82aa11a2fbf
JudgementalBird <judgementalbird@gmail.com>
2025-04-11 16:40:18 +0200

    Set up basic structure

---
+++ Modules/Anim.lua
@@ -0,0 +1,32 @@
+
+---@section __LB_SIMULATOR_ONLY_STORMSL_ANIM_START__
+do
+	local StormSL,ipairs_SL,pairs_SL,insert_SL,remove_SL,type_SL,unpack_SL,Anim=StormSL,ipairs,pairs,table.insert,table.remove,type,table.unpack
+	---@endsection
+	
+	---@section Anim 1 STORMSL_ANIM_CLASS
+	---@class Anim
+	---@field 
+	---Simple, useful animation functions.
+	Anim = {
+		
+		---@section transition_SL
+		---
+		---@param
+		---@param
+		transition_SL=function(start, finish, easing)
+			
+		end,
+		---@endsection
+		
+	}
+	
+	StormSL.Anim = Anim
+	---@endsection STORMSL_ANIM_CLASS
+	
+	
+	
+	
+	---@section __LB_SIMULATOR_ONLY_STORMSL_ANIM_END__
+	end
+	---@endsection
